12b3a8721c8d0227298f70df8f976cdd1f9dcfc1
Egor Danilov <egordanilov@Egors-MBP.hsd1.co.comcast.net>
2022-04-08 20:42:05 -0600

    fully completed branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GlobalFonts from './components/styles/GlobalFonts';
 import CSSReset from './components/styles/CSSReset';
 import GlobalStyles from './components/styles/GlobalStyles';
+import Home from './components/routes/Home';
+import Search from './components/routes/Search';
 
 function App() {
   return (
     <>
-      <CSSReset />
-      <GlobalFonts />
-      <GlobalStyles />
-      <h1>
-        App Placeholder
-      </h1>
+      <Router>
+        <CSSReset />
+        <GlobalFonts />
+        <GlobalStyles />
+        <h1>
+          App Placeholder
+        </h1>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -1,10 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-    body {
-        background-color: red;
-        color: white;
-    }
 `;
 
 export default GlobalStyles;
