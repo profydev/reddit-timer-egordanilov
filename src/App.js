7ff554269b17bf2c7099420e5882cdb42f05faf6
egordanilov <31379969+egordanilov@users.noreply.github.com>
2022-04-10 18:29:42 -0600

    Create app skeleton (#1)
    * Fonts installed and css reset added
    
    * Global styles added
    
    *Folder structure implemented
    
    * Router added
    
    * Changes after review implemented
    
    Co-authored-by: Egor Danilov <egordanilov@Egors-MBP.hsd1.co.comcast.net>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalFonts from './components/styles/GlobalFonts';
+import CSSReset from './components/styles/CSSReset';
+import GlobalStyles from './components/styles/GlobalStyles';
+import Home from './components/routes/Home';
+import Search from './components/routes/Search';
+import NotFound from './components/routes/NotFound';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <CSSReset />
+        <GlobalFonts />
+        <GlobalStyles />
+        <h1>
+          App Placeholder
+        </h1>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
