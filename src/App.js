322ea750014473a77a522784306b884d9e5d20e0
Egor Danilov <egordanilov@Egors-MBP.hsd1.co.comcast.net>
2022-04-10 18:26:20 -0600

    Changes after review

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import GlobalFonts from './components/styles/GlobalFonts';
 import CSSReset from './components/styles/CSSReset';
 import GlobalStyles from './components/styles/GlobalStyles';
 import Home from './components/routes/Home';
 import Search from './components/routes/Search';
+import NotFound from './components/routes/NotFound';
+import theme from './theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router>
         <CSSReset />
         <GlobalFonts />
@@ -20,9 +23,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
-    </>
+    </ThemeProvider>
   );
 }
 
